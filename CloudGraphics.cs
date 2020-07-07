5b72292ff3be05ace16951cc0fcf6629e29e3450
alexMilmore <52534013+alexMilmore@users.noreply.github.com>
2020-07-07 16:40:50 +0100

    Add files via upload

---
+++ CloudGraphics.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGraphics : MonoBehaviour
+{
+    // ALL CONSTANTS
+    Renderer m_Renderer;
+    public ComputeShader compute;
+    RenderTexture result;
+    RenderTexture bumpMap;
+    RenderTexture smoothMap;
+
+    [Header("Textures")]
+    public Texture2D noiseTex;
+    public Texture2D shapeTex;
+
+    [Header("Texture settings")]
+    public int pixWidth;
+    public int pixHeight;
+    [Range(0, 200)]
+    public int xOffset;
+    [Range(0, 200)]
+    public int yOffset;
+    [Range(0.0f, 1.0f)]
+
+    [Header("Cloud shape")]
+    public float NoiseWeight;
+    [Range(0.0f, 1.0f)]
+    public float shapeWeight;
+
+    [Header("Normals")]
+    public bool largerSobel;
+    [Range(0.0f, 100.0f)]
+    public float sharpness;
+    [Range(1, 20)]
+    public int sampleDist;
+    public bool blur;
+    [Range(1, 64)]
+    public int blurDist = 1;
+    public bool viewNormals;
+
+    [Header("Lighting")]
+    public bool lightObject;
+    public bool celShading;
+
+    // delegete functon for generating textures
+    public delegate Color DelegateDeclaration(int a, int b, int c, int d, int e);
+
+    // kernels from compute shader
+    private int kernel;
+    private int normalKernel;
+    private int normalKernel5;
+    private int blurKernel;
+
+    // float for animation
+    private float timeShift;
+
+
+    // ALL FUNCTIONS
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get kernel id
+        kernel = compute.FindKernel("CreateSprite");
+        normalKernel = compute.FindKernel("CalcNormals");
+        normalKernel5 = compute.FindKernel("CalcNormals5");
+        blurKernel = compute.FindKernel("GaussianBlur");
+
+        // create textures
+        result = new RenderTexture(pixHeight, pixWidth, 24);
+        result.enableRandomWrite = true;
+        result.Create();
+        bumpMap = new RenderTexture(pixHeight, pixWidth, 24);
+        bumpMap.enableRandomWrite = true;
+        bumpMap.Create();
+        smoothMap = new RenderTexture(pixHeight, pixWidth, 24);
+        smoothMap.enableRandomWrite = true;
+        smoothMap.Create();
+
+        // create delegates (this is for passing functions through the gentex Function)
+        DelegateDeclaration noiseCol = PerlinNoiseColour;
+        DelegateDeclaration shapeCol = ShapeColour;
+
+        // THIS SHIT CHOKES THE COMPUTER THE FUCK OUT, NEED TO FIX
+        // generate noise texture
+        //noiseTex = new Texture2D(pixWidth * 2, pixHeight * 2);
+        //noiseTex = GenTex(noiseTex, noiseCol);
+
+        // generate basic shape
+        //shapeTex = new Texture2D(pixWidth, pixHeight);
+        //shapeTex = GenTex(shapeTex, shapeCol);
+
+        // set textures in compute shader
+        compute.SetTexture(kernel, "Result", result);
+        compute.SetTexture(kernel, "Noise", noiseTex);
+        compute.SetTexture(kernel, "Shape", shapeTex);
+
+        // run compute shader
+        compute.Dispatch(kernel, pixWidth / 8, pixHeight / 8, 1);
+
+        // get renderer
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (CheckVisible()) {
+        // set offset values in noise texture
+        float[] offset = { xOffset, yOffset };
+        compute.SetFloats(Shader.PropertyToID("offset"), offset);
+        compute.SetFloat(Shader.PropertyToID("noiseWeight"), NoiseWeight);
+        compute.SetFloats(Shader.PropertyToID("shapeWeight"), shapeWeight);
+        timeShift = Mathf.Sin(Time.time);
+        compute.SetFloats(Shader.PropertyToID("timeShift"), timeShift);
+        compute.SetFloat(Shader.PropertyToID("sharpness"), sharpness);
+        compute.SetInt(Shader.PropertyToID("dist"), sampleDist);
+
+        // run compute shader
+        compute.Dispatch(kernel, pixWidth / 8, pixHeight / 8, 1);
+
+        ComputeNormals();
+
+        // Mean blur to smooth out normals
+        if (blur == true)
+        {
+            SmoothNormals(blurDist);
+        }
+
+        // Send to renderer
+        if (viewNormals == false)
+        {
+            m_Renderer.material.SetTexture("_MainTex", result);
+            m_Renderer.material.SetTexture("_BumpMap", bumpMap);
+        }
+        else
+        {
+            m_Renderer.material.SetTexture("_MainTex", bumpMap);
+            Texture2D blank = new Texture2D(bumpMap.width, bumpMap.height);
+            m_Renderer.material.SetTexture("_BumpMap", blank);
+        }
+      }
+    }
+
+    bool CheckVisible()
+    {
+        GameObject player = GameObject.Find("Player");
+        Vector3 distance = transform.position - player.transform.position;
+        if ((Mathf.Abs(distance.y) < 7 + (transform.localScale.y / 2)) && (Mathf.Abs(distance.x) < 12 + (transform.localScale.x / 2)))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void ComputeNormals()
+    {
+        if (largerSobel == true)
+        {
+            // send computed image to shader to calculate normals
+            compute.SetTexture(normalKernel5, "Result", result);
+            compute.SetTexture(normalKernel5, "BumpMap", bumpMap);
+
+            // run compute shader
+            compute.Dispatch(normalKernel5, pixWidth / 8, pixHeight / 8, 1);
+        }
+        else
+        {
+            // send computed image to shader to calculate normals
+            compute.SetTexture(normalKernel, "Result", result);
+            compute.SetTexture(normalKernel, "BumpMap", bumpMap);
+
+            // run compute shader
+            compute.Dispatch(normalKernel, pixWidth / 8, pixHeight / 8, 1);
+        }
+    }
+
+    void SmoothNormals(int distance)
+    {
+        compute.SetTexture(blurKernel, "BumpMap", bumpMap);
+        compute.SetTexture(blurKernel, "SmoothMap", smoothMap);
+        compute.SetInt(Shader.PropertyToID("blurSize"), distance);
+
+        // run compute shader
+        compute.Dispatch(blurKernel, pixWidth / 8, pixHeight / 8, 1);
+    }
+
+    int FindIndex(int x, int y, int width)
+    {
+        return y * width + x;
+    }
+
+    Texture2D GenTex(Texture2D tex, DelegateDeclaration handler)
+    {
+        Color[] colourMem = new Color[tex.height * tex.width];
+        int noiseScale = 10;
+        int y = 0;
+        while (y < tex.height)
+        {
+            int x = 0;
+            while (x < tex.width)
+            {
+                colourMem[FindIndex(x, y, tex.width)] = handler(x, y, tex.width, tex.height, noiseScale);
+                x++;
+            }
+            y++;
+        }
+
+        // Copy the pixel data to the texture and load it into the GPU.
+        tex.SetPixels(colourMem);
+        tex.Apply();
+        return tex;
+    }
+
+    Color PerlinNoiseColour(int x, int y, int noiseWidth, int noiseHeight, int noiseScale)
+    {
+        float xCoord = (float)x / noiseWidth * noiseScale;
+        float yCoord = (float)y / noiseHeight * noiseScale;
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+
+    Color ShapeColour(int x, int y, int width, int height, int const1)
+    {
+        float angle = Mathf.Sin((float)x / width * Mathf.PI) * Mathf.Sin((float)y / height * Mathf.PI);
+        return new Color(angle, angle, angle);
+    }
+}
---
+++ CloudShader.shader
@@ -0,0 +1,111 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Unlit/CloudShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_BumpMap("Bumpmap", 2D) = "bump" {}
+		_ColorTint("Tint", Color) = (1.0, 1.0, 1.0, 1.0)
+		_ShadowTint("Tint", Color) = (1.0, 1.0, 1.0, 1.0)
+		_BaseLight("Base Luminosity", Range(0, 1)) = 0.5
+		_CelShading("Cel Shade", Float) = 1
+		_Cutoff("Effect Amount", Range(0, 1)) = 0.01
+		_CelShadingAlpha("Cel Shade Alpha", Float) = 1
+		_CutoffAlpha("Effect Amount", Range(0, 1)) = 0.01
+		_Outline("Include outline", Float) = 0
+		_OutlineThickness("Outline Thickness", Range(0, 1)) = 0
+    }
+    SubShader
+    {
+        Tags { "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent"}
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+			uniform float _BaseLight;
+			uniform float _CelShading;
+			uniform float _Cutoff;
+			uniform float _CelShadingAlpha;
+			uniform float _CutoffAlpha;
+			fixed4 _ColorTint;
+			fixed4 _ShadowTint;
+			uniform float _Outline;
+			uniform float _OutlineThickness;
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+				float4 worldSpacePos : TEXCOORD1;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+			sampler2D _BumpMap;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.worldSpacePos = mul(unity_ObjectToWorld, v.vertex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+				// sample normal map
+				half3 tNormal = UnpackNormal(tex2D(_BumpMap, i.uv));
+				// calculate light
+        float3 distanceFromLight = float3(_WorldSpaceLightPos0.x - i.worldSpacePos.x, _WorldSpaceLightPos0.y - i.worldSpacePos.y, 0);
+				float attenuation = 1 / length(distanceFromLight);
+				half lightIntensity = max(0, dot(tNormal, distanceFromLight)) * attenuation;
+				if (_CelShading == 1) {
+					lightIntensity = smoothstep(0 + _Cutoff, 0.01 + _Cutoff, lightIntensity);
+				}
+
+				// Allow cel shading for Alpha
+				if (_CelShadingAlpha == 1) {
+					col.a = smoothstep(0 + _CutoffAlpha, 0.01 + _CutoffAlpha, col.a);
+				}
+
+
+				// Allow outline
+				if (_Outline == 1) {
+					col.rgb = col.rgb * _ColorTint * lightIntensity + col.rgb * _ShadowTint * (1 - lightIntensity);
+					//col.rgb = col.rgb;
+
+					col.rgb = col.rgb * smoothstep(_OutlineThickness, 1, col.a);
+				}
+				else {
+					col.rgb = col.rgb * _ColorTint * lightIntensity + col.rgb * _ShadowTint * (1 - lightIntensity);
+					//col.rgb = col.rgb;
+				}
+
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
